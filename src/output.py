41705a5b226a88bc7ddfe33532ce44755244a576
Hajime Fujita <hajime.fujita@gmail.com>
2022-06-22 16:49:09 +0900

    HTMLのタイトルを出力できるようにした

--- src/output.py
+++ src/output.py
@@ -1,10 +1,19 @@
 from jinja2 import Environment, FileSystemLoader
+from datetime import datetime as dt
 
 def output_html(meishiki, unsei):
     
+    env = Environment(loader = FileSystemLoader('html/'))
+    template = env.get_template('template.html')
     
+    birthday_str = meishiki.birthday.strftime('%Y年%-m月%-d日 %-H時%-M分生')
+    sex_str = '男命' if meishiki.sex == 0 else '女命'
     
+    content = {'birthday': birthday_str, 'sex': sex_str}
     
+    result = template.render(content)
+    with open('html/' + meishiki.birthday.strftime('%Y_%m%d_%H%M_') + str(meishiki.sex) + '.html', 'w') as f:
+        f.write(result)
     
     return 1
 
